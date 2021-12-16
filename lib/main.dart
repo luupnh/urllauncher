42b09a023ba3f10d6030c80cfa72a156bbd889b6
hailuu.1999@gamil.com <Hailuu123@>
2021-12-16 13:53:56 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+const String _url = 'https://offwork.teqnological.asia/activity/announcements/841';
+
+void main() => runApp(
+  const MaterialApp(
+    home: Material(
+      child: Center(
+        child: RaisedButton(
+          onPressed: _launchURL,
+          child: Text('Show Flutter homepage'),
+        ),
+      ),
+    ),
+  ),
+);
+
+void _launchURL() async {
+  if (!await launch(_url)) throw 'Could not launch $_url';
+}
